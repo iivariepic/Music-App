5703abf8a25ddae9b5dd14f1b48f977772473485
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2024-11-21 15:50:44 +0200

    Added basic album model (not migrated yet)

--- music_app/models.py
+++ music_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 
-# Create your models here.
+
+class Album(models.Model):
+    """A model for albums"""
+    name = models.CharField(max_length=300)
+    release_year = models.IntegerField(default=1900,
+                    validators=[
+                        MaxValueValidator(datetime.datetime.now().year()), 
+                        MinValueValidator(1900)])
