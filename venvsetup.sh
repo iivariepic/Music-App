ee391c9743aa3e971275db12a814536b15dc9af6
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2024-11-29 11:41:06 +0200

    Modified setup files to the venv file name

--- venvsetup.sh
+++ venvsetup.sh
@@ -1,10 +1,10 @@
 # Create the virtual environment
 echo "Creating virtual environment..."
-virtualenv .ma_env
+virtualenv ma_env
 
 # Activate the virtual environment
 echo "Activating virtual environment..."
-source .ma_env/bin/activate
+source ma_env/bin/activate
 
 # Install dependencies
 echo "Installing dependencies from requirements.txt..."
